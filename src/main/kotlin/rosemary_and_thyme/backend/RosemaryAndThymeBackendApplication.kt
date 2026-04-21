25ecf01acc5413837c4eb8f6d17ccf79041c5fd8
zacnoa <zacnoa2.4@gmail.com>
2026-04-21 23:47:40 +0200

    second commit

---
+++ src/main/kotlin/rosemary_and_thyme/backend/RosemaryAndThymeBackendApplication.kt
@@ -0,0 +1,11 @@
+package rosemary_and_thyme.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RosemaryAndThymeBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<RosemaryAndThymeBackendApplication>(*args)
+}
---
+++ src/test/kotlin/rosemary_and_thyme/backend/RosemaryAndThymeBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package rosemary_and_thyme.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RosemaryAndThymeBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
